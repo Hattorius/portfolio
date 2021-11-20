58be758f4790f952fb4cc0d011fc5fb0bbbfba26
Aaron Jonk <AJ@jonk.name>
2021-11-21 00:38:48 +0100

    Init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+    background: hotpink;
+`;
+
+export const App = (() => {
+    return (
+        <Wrapper>Hello world!</Wrapper>
+    );
+})
