9eae51e5dadd74f375b4364b1dc4ea2ba3357c07
Aaron Jonk <AJ@jonk.name>
2022-06-15 21:56:12 +0200

    shit

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
-import React from "react";
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+    html, body {
+        width: 100vw;
+        min-height: 100vh;
+        margin: 0;
+        padding: 0;
+        font-family: sans-serif, monospace;
+        overflow-x: hidden;
+    }
+    * {
+        box-sizing: border-box;
+    }
+`;
 
 export const App = () => {
 	return (
-		<>Hello World!</>
+		<>
+            <GlobalStyle />
+            Hello World!
+        </>
 	);
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,5 @@
 import React from "react";
 import { render } from "react-dom";
 import { App } from "./App";
-import { createGlobalStyle } from "styled-components";
 
-const GlobalStyle = createGlobalStyle`
-    html, body {
-        width: 100vw;
-        min-height: 100vh;
-        margin: 0;
-        padding: 0;
-        font-family: sans-serif, monospace;
-        overflow-x: hidden;
-    }
-    * {
-        box-sizing: border-box;
-    }
-`;
-
-render(<App />, document.getElementById('root'));
+render(<App/>, document.getElementById('root'));
